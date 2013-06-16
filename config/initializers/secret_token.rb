4451a6dd245a4439627b0b6a1f6b32cd3175f856
emerson <emerson.frontender@gmail.com>
2013-06-16 23:21:59 +0200

    App folder was made

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GericoTickets::Application.config.secret_token = 'b48f71e2d0d26bee9fafa78ba00f51db4181bf7b5143ee00db2869b30fd38188f91ed3b5bb49ca6373257b3f2011ce0dd898f2d5960e692019f84d2ac30a2189'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GericoTickets::Application.config.session_store :cookie_store, key: '_gerico_tickets_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GericoTickets::Application.config.session_store :active_record_store
