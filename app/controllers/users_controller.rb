7f3740bed524cddbdf15853d64d9306cc5eaa402
emerson <emerson.frontender@gmail.com>
2013-06-17 00:43:15 +0200

    views for index, new, and show where made

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
+  def index
+  	@users = User.all 
+  end
+  
+  def create
+  	@user = User.create
+  end
+  
   def new
+  	@user = User.new
+  	@user.save
+  end
+  
+  def edit
+  end
+  
+  def show
+  	@user = User.find(params[:id])
+  end
+  
+  def update
+  end
+
+  def destroy
   end
 end
