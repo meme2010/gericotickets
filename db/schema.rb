57739c5d258241110d5af34b00b9614aef05bf67
meme2010 <emerson.frontender@gmail.com>
2013-06-18 11:04:06 +0200

    no changes where made

--- db/schema.rb
+++ db/schema.rb
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-# encoding: UTF-8
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended to check this file into your version control system.
-
-ActiveRecord::Schema.define(:version => 20130617104937) do
-
-  create_table "events", :force => true do |t|
-    t.string   "titel"
-    t.string   "text"
-    t.integer  "tickets"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "users", :force => true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-    t.string   "password_digest"
-  end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-
-end
-=======
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -83,4 +47,3 @@ ActiveRecord::Schema.define(:version => 20130617201250) do
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
->>>>>>> 39e5973e501c20b9971e88d27a20449c3d2b0f96
